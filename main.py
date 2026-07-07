8132d20de55f4450866ccb8666e9fa24b60a59ab
manu <298598440+scaryskywalker@users.noreply.github.com>
2026-07-07 10:30:53 +0530

    1

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+from fastapi import FastAPI
+
+api = FastAPI()
+
+@api.get("/")
+def index():
+    return {"message": "Hello, World!"}
+
+
+@api.get("/calculations/{a}")
+def calc(a: int):
+    return {"result": a * 2}
