6ece366809815f437ad9c632abe64e6e56511417
spy-lakshit <lakshitjoshi2018@gmail.com>
2026-07-08 14:11:53 +0530

    Delete prototype directory

--- prototype/url-shortener-backend/app/services/api_key_service.py
+++
@@ -1,6 +0,0 @@
-from app.core.security import generate_api_key, hash_api_key
-
-
-def create_api_key_pair() -> tuple[str, str]:
-    api_key = generate_api_key()
-    return api_key, hash_api_key(api_key)
